db6fee51c3b9ad7003c4d918d86562e9b51cc224
Aryan <aryansaini8773@gmail.com>
2026-06-07 10:50:41 +0530

    Learned Dutch flag Algo

--- Arrays/src/DutchNationalFlagAlgo.java
+++ Arrays/src/DutchNationalFlagAlgo.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class DutchNationalFlagAlgo {
+    // format: [0....low-1] [low....mid-1]                      [mid.....high]     [high....high+1]
+    //         /        /     /        /                         /          /         /          /
+    //        0 0 0 0..0     1 1 1 ... 1 (till here sorted)|    2/0/1/2(unsorted)    2 2 ...... 2 (sorted)
+    //
+    // if (arr[mid]==0) --> swap(low, mid) and move low++, mid++
+    // if (arr[mid]==1) --> till [0,mid] it will be already sorted mid++;
+    // if (arr[mid]==2) --> swap(mid, high) only high--;
+
+//    three pointers , low=0, mid = 0, high =n-1;
+//            low
+//             /
+//    arr[] = [0,1,2,0,1,2,1,2,0,0,0,1]
+//             \                      \
+//              mid                   high
+
+    static void sortZOT(int[] arr){
+        int n = arr.length;
+        int low = 0, mid = 0, high = n-1;
+        while (mid<=high){
+            if (arr[mid]==0){
+                swap(arr, low, mid);
+                mid++;
+                low++;
+            }
+            else if (arr[mid]==1) mid++;
+            else{
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+    static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0,1,2,0,1,2,1,2,0,0,0,1};
+        sortZOT(arr);
+        for (int i:arr){
+            System.out.print(i+" ");
+        }
+    }
 }
