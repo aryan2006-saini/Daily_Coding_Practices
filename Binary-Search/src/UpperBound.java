9b0896b1068e1f10bf448fa2a03e951ed3289af2
Aryan <aryansaini8773@gmail.com>
2026-06-15 20:42:40 +0530

    Solved new Problem

--- Binary-Search/src/UpperBound.java
+++ Binary-Search/src/UpperBound.java
@@ -1,11 +1,10 @@
-public class LowerBound {
-    // smallest index such that arr[index]>=x
-    // 1. Brute force
-    static int loweBoundBrute(int[] arr, int x){
+public class UpperBound {
+    // smallest index such that arr[index]>x
+    static int upperBoundBrute(int[] arr, int x){
         int n = arr.length;
         int res = n;
-        for (int i=0; i<n; i++){
-            if (arr[i]>=x){
+        for (int i = 0; i<n; i++){
+            if (arr[i]>x){
                 res = i;
                 break;
             }
@@ -13,39 +12,38 @@ public class LowerBound {
         return res;
     }
     // Using Binary Search
-    static int loweBound1(int[] arr, int x){
+    static int upperBound1(int[] arr, int x){
         int n = arr.length;
         int low = 0, high = n-1;
         int res = n;
         while (low<=high){
             int mid = low +(high-low)/2;
-            if (arr[mid]>=x){
+            if (arr[mid]>x){
                 res = mid;
                 high = mid-1;
             }
-            else low = mid +1;
+            else low = mid+1;
         }
         return res;
     }
-    //without the extra res variable
-    static int loweBound2(int[] arr, int x){
+    // without taking extra res variable
+    static int upperBound2(int[] arr, int x){
         int n = arr.length;
         int low = 0, high = n-1;
         while (low<=high){
             int mid = low +(high-low)/2;
-            if (arr[mid]>=x){
+            if (arr[mid]>x){
                 high = mid-1;
             }
-            else low = mid +1;
+            else low = mid+1;
         }
         return low;
     }
-
     public static void main(String[] args) {
-        int[] arr = {1,2,4,4,4,5,7,8,9};
-        int x= 4;
-        System.out.println(loweBoundBrute(arr,x));
-        System.out.println(loweBound1(arr,x));
-        System.out.println(loweBound2(arr,x));
+        int[] arr = {1,2,2,3,5,6,6,6,7,7,8};
+        int x=6;
+        System.out.println(upperBoundBrute(arr,x));
+        System.out.println(upperBound1(arr,x));
+        System.out.println(upperBound2(arr,x));
     }
 }
