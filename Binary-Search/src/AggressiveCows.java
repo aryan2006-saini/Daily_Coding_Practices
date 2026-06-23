2adc4b6047198eb067654cfa17c8e1c89190049d
Aryan <aryansaini8773@gmail.com>
2026-06-23 21:29:52 +0530

    Solved new problem

--- Binary-Search/src/AggressiveCows.java
+++ Binary-Search/src/AggressiveCows.java
@@ -16,15 +16,35 @@ public class AggressiveCows {
         int[] a = arr.clone();
         int n = a.length;
         Arrays.sort(a);
-        for (int i=1; i<(a[n-1]-a[0]); i++){
-
+        int to = a[n-1]-a[0];
+        for (int i=1; i<to; i++){
+            if (canWePlace(arr,i,cows)==true){
+                continue;
+            }
+            else{
+                return (i-1);
+            }
         }
+        return -1;
     }
 
-
-
-
-
+    // 2. Optimal Approach (Binary Search)
+    static int aggressiveCows(int[] arr, int cows){
+        int[] ar = arr.clone();
+        int n = arr.length;
+        Arrays.sort(ar);
+        int low = 1, high = ar[n-1]-ar[0];
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (canWePlace(ar, mid, cows)==true){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return high;
+    }
     // can we place?
     static boolean canWePlace(int[] arr, int distance, int cows){
         int n = arr.length;
@@ -37,5 +57,12 @@ public class AggressiveCows {
             if (countCows>=cows) return true;
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 3,  4,  7, 9, 10};
+        int cows = 4;
+        System.out.println(aggressiveCowsBrute(arr,cows));
+        System.out.println(aggressiveCows(arr,cows));
     }
 }
