e323c4dcdc49610371e83ea87f52eb53b7de7ba8
Aryan <aryansaini8773@gmail.com>
2026-07-16 22:38:17 +0530

    Solved new problem

--- Two-Pointers-and-Sliding-Window/src/Patterns/ConstantWindow.java
+++ Two-Pointers-and-Sliding-Window/src/Patterns/ConstantWindow.java
@@ -4,24 +4,23 @@ public class ConstantWindow {
 
     // problem - given an array return max window sum of length k
 
-    static int maximumSum(int[] arr, int k){
+    static int maximumSum(int[] arr, int k) {
         int n = arr.length;
         int sum = 0;
-        for (int i=0; i<k; i++){
-            sum+=arr[i];
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
         }
         int maxSum = Integer.MIN_VALUE;
-        int l = 0, r = k-1;
-        while (r<n-1){
+        int l = 0, r = k - 1;
+        while (r < n - 1) {
             sum -= arr[l];
             l++;
             r++;
-            sum+= arr[r];
+            sum += arr[r];
             maxSum = Math.max(maxSum, sum);
         }
         return maxSum;
     }
-
     public static void main(String[] args) {
         int[] arr = {-1, 2, 3, 3, 4, 5, -1};
         int k = 4;
