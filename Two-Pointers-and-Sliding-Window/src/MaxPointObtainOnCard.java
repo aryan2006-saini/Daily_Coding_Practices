1ebe22756c65a02dda2da47221d822a7e3e6bdfd
Aryan <aryansaini8773@gmail.com>
2026-07-22 00:00:04 +0530

    Solved new problem

--- Two-Pointers-and-Sliding-Window/src/MaxPointObtainOnCard.java
+++ Two-Pointers-and-Sliding-Window/src/MaxPointObtainOnCard.java
@@ -1,3 +1,21 @@
 public class MaxPointObtainOnCard {
+    // Given array of card and integer k, we have to take cards either from left or right or from both side,
+    // but in a manner.
+    static int maxPoint(int[] arr,int k ){
+        int n = arr.length;
+        int lSum = 0, rSum = 0, maxSum = 0;
 
+        for (int i=0; i<k; i++){
+            lSum += arr[i];
+        }
+        maxSum = lSum;
+        int rIdx = n-1;
+        for (int i=k-1; i>=0; i--){
+            lSum -= arr[i];
+            rSum += arr[rIdx];
+            rIdx--;
+
+            maxSum
+        }
+    }
 }
